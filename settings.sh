cb367b2653275f2252ebee8839b0a6da964e4c41
bob chase <bob.chase@gmail.com>
2024-04-09 21:51:04 -0400

    take homebrew out of basic PATH

--- settings.sh
+++ settings.sh
@@ -1,5 +1,5 @@
 export PS1="%m:%20<...<%~%<<$ "
-export PATH=$HOME/bin:/opt/homebrew/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$HOME/.fzf/bin
+export PATH=$HOME/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$HOME/.fzf/bin
 export SHELL=/bin/bash
 export LS_OPTIONS='-G'
 

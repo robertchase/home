101da2538f9c13c30105763a319e02a117f08c16
bob chase <bob.chase@gmail.com>
2024-04-11 21:48:24 -0400

    add pip-tree

--- pip.sh
+++ pip.sh
@@ -11,3 +11,20 @@ function pip-required-by {
         }
     ' | grep $1
 }
+
+function pip-tree {
+    pip freeze |
+    awk -F\= '{print $1}' |
+    xargs pip show |
+    awk -F: '
+        $1 == "Name" {
+            name = $2
+        }
+        $1 == "Required-by" {
+            split($2, reqs, ",")
+            for (req in reqs) {
+                print name, reqs[req]
+            }
+        }
+    '
+}
